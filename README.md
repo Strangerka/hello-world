5adfe46eba81e43562d1334f2b0e15344dba09a5
Barbora Kečkéšová <keckesova@azet-sk.sk>
2017-07-11 17:15:07 +0200

    prvy commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# hello-world
-Hello
+# Use to print Hello World!
---
+++ src/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: keckesova
+ * Date: 11.7.2017
+ * Time: 16:00
+ */
+
+class HelloWorld
+{
+    public function print(){
+        printf("Hello World!");
+    }
+}
